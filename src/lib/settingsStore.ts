335ee18725eb7a9f519fc0fc520dbbc8b24dda57
thinksamwp <Sam.Walker-Penn@thinkology.co.uk>
2023-04-17 18:57:55 +0100

    Update settings store to work with optional settings

--- src/lib/settingsStore.ts
+++ src/lib/settingsStore.ts
@@ -8,57 +8,69 @@ function createSettingsStore() {
 
 	function updateSetting<T extends SettingsKey>(section: T, setting: SectionKey<T>, value: any) {
 		update((settings) => {
-			settings[section][setting] = value;
+			const newSettings = { ...settings };
+
+			if (!newSettings['[Render.13]']) {
+				newSettings['[Render.13]'] = {};
+			}
+
+			const newSection = { ...(newSettings[section] ?? ({} as any)) };
+
+			newSection[setting] = value;
+			newSettings[section] = newSection;
 
 			if (setting === 'WindowMode') {
-				settings['[Render.13]'].WindowedFullscreen = 0;
-				settings['[Render.13]'].FullscreenWindow = 0;
-				settings['[Render.13]'].FullscreenWindowEnabled = 0;
+				newSettings['[Render.13]'].WindowedFullscreen = 0;
+				newSettings['[Render.13]'].FullscreenWindow = 0;
+				newSettings['[Render.13]'].FullscreenWindowEnabled = 0;
 
 				switch (value) {
 					case 0:
-						settings['[Render.13]'].FullscreenWindowEnabled = 1;
+						newSettings['[Render.13]'].FullscreenWindowEnabled = 1;
 						break;
 					case 1:
-						settings['[Render.13]'].WindowedFullscreen = 1;
+						newSettings['[Render.13]'].WindowedFullscreen = 1;
 						break;
 					case 2:
-						settings['[Render.13]'].FullscreenWindow = 1;
+						newSettings['[Render.13]'].FullscreenWindow = 1;
 						break;
 				}
 			} else if (setting === 'UseCustomWorldScale' && value === 0) {
-				settings['[Render.13]'].MinWorldScale = 50;
-				settings['[Render.13]'].MaxWorldScale = 100;
+				newSettings['[Render.13]'].MinWorldScale = 50;
+				newSettings['[Render.13]'].MaxWorldScale = 100;
 			} else if (setting === 'UseCustomFrameRates' && value === 0) {
-				settings['[Render.13]'].FrameRateCap = 60;
+				newSettings['[Render.13]'].FrameRateCap = 60;
 			} else if (setting === 'GFXPresetLevel') {
-				settings['[Render.13]'].TextureDetail = value >= 3 ? 3 : value;
-				settings['[Render.13]'].AnisotropicFiltering = value >= 2 ? 1 : 0;
-				settings['[Render.13]'].MaxAnisotropy =
+				newSettings['[Render.13]'].TextureDetail = value >= 3 ? 3 : value;
+				newSettings['[Render.13]'].AnisotropicFiltering = value >= 2 ? 1 : 0;
+				newSettings['[Render.13]'].MaxAnisotropy =
 					value === 5 ? 16 : value === 4 ? 8 : value === 3 ? 4 : value === 2 ? 2 : 1;
-				settings['[Render.13]'].MaxEffectsAnisotropy =
+				newSettings['[Render.13]'].MaxEffectsAnisotropy =
 					value === 5 ? 8 : value === 4 ? 4 : value === 3 ? 2 : 1;
-				settings['[Render.13]'].MaxExtraQualityAnisotropy =
+				newSettings['[Render.13]'].MaxExtraQualityAnisotropy =
 					value === 5 ? 16 : value === 4 ? 16 : value === 3 ? 8 : value === 2 ? 4 : 1;
-				settings['[Render.13]'].LocalFogDetail = value >= 4 ? 4 : value;
-				settings['[Render.13]'].SimpleDirectionalShadows = value >= 3 ? 3 : value;
-				settings['[Render.13]'].DirectionalShadowDetail = value >= 4 ? 4 : value;
-				settings['[Render.13]'].ModelQuality = value >= 4 ? 4 : value;
-				settings['[Render.13]'].EffectsQuality = value >= 4 ? 4 : value;
-				settings['[Render.13]'].LightQuality = value >= 4 ? 4 : value;
-				settings['[Render.13]'].AADetail = value >= 4 ? 4 : value;
-				settings['[Render.13]'].RefractionDetail = value >= 3 ? 3 : value;
-				settings['[Render.13]'].SSAODetail = value >= 3 ? 3 : value;
-				settings['[Render.13]'].LocalReflections = value >= 3 ? 1 : 0;
+				newSettings['[Render.13]'].LocalFogDetail = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].SimpleDirectionalShadows = value >= 3 ? 3 : value;
+				newSettings['[Render.13]'].DirectionalShadowDetail = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].ModelQuality = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].EffectsQuality = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].LightQuality = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].AADetail = value >= 4 ? 4 : value;
+				newSettings['[Render.13]'].RefractionDetail = value >= 3 ? 3 : value;
+				newSettings['[Render.13]'].SSAODetail = value >= 3 ? 3 : value;
+				newSettings['[Render.13]'].LocalReflections = value >= 3 ? 1 : 0;
 			} else if (setting === 'MaxAnisotropy') {
-				settings['[Render.13]'].AnisotropicFiltering = value >= 2 ? 1 : 0;
-				settings['[Render.13]'].MaxEffectsAnisotropy =
+				newSettings['[Render.13]'].AnisotropicFiltering = value >= 2 ? 1 : 0;
+				newSettings['[Render.13]'].MaxEffectsAnisotropy =
 					value === 16 ? 8 : value === 8 ? 4 : value === 4 ? 2 : 1;
-				settings['[Render.13]'].MaxExtraQualityAnisotropy =
+				newSettings['[Render.13]'].MaxExtraQualityAnisotropy =
 					value >= 8 ? 16 : value >= 4 ? 8 : value >= 2 ? 4 : 1;
 			}
 
+			settings = newSettings;
+
 			invoke('write_settings', { settings: prepareSettings(settings) });
+
 			return settings;
 		});
 	}
